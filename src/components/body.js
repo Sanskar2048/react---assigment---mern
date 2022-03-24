b347cb893860df85923545141c5d87f34a6433d0
sanskar2048 <ss9814@srmist.edu.in>
2022-03-24 23:10:41 +0530

    first commit

---
+++ src/components/body.js
@@ -0,0 +1,239 @@
+import React from "react";
+import '../css/body.css';
+
+function Body() {
+    return (
+        <div className="Body-Contaner">
+            <h4>College of Engineering and Technology, SRM Institute of Science and Technology, Kattankulathur.
+                Academic Status</h4>
+            <div className="heading">
+                Academic Status
+            </div>
+            <div className="image-Contaner">
+                <img src={process.env.PUBLIC_URL + "2.jpg"} width="15%" height="13%" />
+                <img src={process.env.PUBLIC_URL + "1.jpg"} width="15%" height="13%" />
+                <img src={process.env.PUBLIC_URL + "3.jpg"} width="15%" height="13%" />
+            </div>
+            <div className="status">
+                <p>Registration Number:</p><p>RA1911003010853</p><br />
+                <p>Name:</p><p>SANSKAR SARAWGI</p><br />
+                <p>Program:</p><p>B.Tech</p><br />
+                <p>Department:</p><p>Computer Science and Engineering</p><br />
+                <p>Specialization:</p><p>CSE</p><br />
+                <p>Semester:</p><p>6</p><br />
+                <p>Feedback Status:</p><p>Completed</p><br />
+                <p>Enrollment Status / DOE:</p><p>true / 10-Jan-22</p><br />
+            </div>
+            <div>
+                <table >
+                    <tr>
+                        <th>Course Code</th>
+                        <th>Course Title</th>
+                        <th>Category</th>
+                        <th>Faculty Name</th>
+                        <th>Slot</th>
+                        <th>Hours Conducted</th>
+                        <th>Hours Absent</th>
+                        <th>Attn %</th>
+                        <th>University Practical Details </th>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J Regular</td>
+                        <td>Database Management Systems</td>
+                        <td>Theory</td>
+                        <td>Dr.V.Deepan Chakravarthy (100665)</td>
+                        <td>A</td>
+                        <td>19</td>
+                        <td>2</td>
+                        <td>89.47</td>
+                    </tr>
+                </table>
+            </div>
+            <div className="bottom">
+                Internal Marks Detail will be updated after each assessment has been conducted.
+            </div>
+            <div className="marks">
+                <table >
+                    <tr>
+                        <th>Course Code</th>
+                        <th>Course Type</th>
+                        <th>Test Performance</th>
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                    <tr>
+                        <td>18CSC303J</td>
+                        <td>Theory</td>
+                        <td>CLA T1 /5.00
+3.40</td>
+
+                    </tr>
+                </table>
+            </div>
+
+        </div>
+    );
+}
+
+export default Body;
